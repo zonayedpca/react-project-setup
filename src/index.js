37b15894d6eda552eb01fda83721948f83bef318
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-08-29 19:38:57 +0530

    how to render into dom

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>My First React Project</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>My First React Project</h1>, document.getElementById('renderhere'));
 registerServiceWorker();
