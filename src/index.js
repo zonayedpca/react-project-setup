a662403bb2066bdbad6bfab008fdb4d06ba3406c
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-08-29 19:11:24 +0530

    Project Setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<h1>My First React Project</h1>, document.getElementById('root'));
+registerServiceWorker();
